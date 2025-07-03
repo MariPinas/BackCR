d9e2f296c65e3070e0c71ae84f08e11d9c3beb09
MariPinas <marianapls2003@gmail.com>
2025-07-03 14:33:03 -0300

    :sparkles: validadeTime

--- src/middlewares/validateTime.ts
+++ src/middlewares/validateTime.ts
@@ -1,1 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+export function validateTime(req: Request, res: Response, next: NextFunction) {
+  const { time } = req.body;
+  if (!/^\d{2}:\d{2}$/.test(time)) {
+    return res.status(400).json({ error: "Hora inválida" });
+  }
+  next();
+}
