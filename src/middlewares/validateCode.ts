d21a1cc4af461990098775f5ef89ed474889e450
MariPinas <marianapls2003@gmail.com>
2025-07-03 14:39:25 -0300

    :sparkles: validadeCode

--- src/middlewares/validateCode.ts
+++ src/middlewares/validateCode.ts
@@ -1,1 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+export function validateCode(req: Request, res: Response, next: NextFunction) {
+  const { code } = req.body;
+  if (!code || code.length !== 6) {
+    return res.status(400).json({ error: "Código inválido" });
+  }
+  next();
+}
