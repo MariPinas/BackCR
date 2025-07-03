a43e074fe3c82f27466580aad2ee7e7b4f095c59
MariPinas <marianapls2003@gmail.com>
2025-07-03 15:49:42 -0300

    :sparkles: add cors

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import { validateDate } from "./middlewares/validateDate";
 import { validateTime } from "./middlewares/validateTime";
 import { validateCode } from "./middlewares/validateCode";
 import { login } from "./controller/AuthController";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post(
   "/login",
